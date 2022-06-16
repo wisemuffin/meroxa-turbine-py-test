4aaf651efb1dad0197779a85698bc1504385a745
David Griffiths <davidgg777@hotmail.com>
2022-06-17 08:36:39 +1000

    updating docs

--- sql/tasks.sql
+++ sql/tasks.sql
@@ -1,3 +1,5 @@
+drop table tasks
+;
 CREATE TABLE IF NOT EXISTS tasks (
     task_id INT AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
@@ -6,5 +8,12 @@ CREATE TABLE IF NOT EXISTS tasks (
     due_date DATE,
     priority TINYINT NOT NULL DEFAULT 3,
     description TEXT,
+    update_at TIMESTAMP,
     PRIMARY KEY (task_id)
 );
+
+INSERT INTO tasks(title, priority, start_date, due_date, email, description, update_at)
+VALUES
+	('1th task', 1, CURRENT_DATE(), CURRENT_DATE(), 'dave@fake.com', 'get some food', CURRENT_TIMESTAMP()),
+	('2th task', 1, CURRENT_DATE(), CURRENT_DATE(), 'emma@fake.com', 'make dinner', CURRENT_TIMESTAMP()), 
+	('3th task', 1, CURRENT_DATE(), CURRENT_DATE(), 'whoop@fake.com', 'clean the house', CURRENT_TIMESTAMP());
--- sql/tasks_insert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tasks(title, priority, start_date, due_date, email, description)
-VALUES
-	('1th task', 1, CURRENT_DATE(), CURRENT_DATE(), 'dave@fake.com', 'get some food'),
-	('2th task', 1, CURRENT_DATE(), CURRENT_DATE(), 'emma@fake.com', 'make dinner'), 
-	('3th task', 1, CURRENT_DATE(), CURRENT_DATE(), 'whoop@fake.com', 'clean the house');
